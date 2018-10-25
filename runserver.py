075e6528526cd007a2950231f05c644349679fbb
shooke30 <shooke_30@sina.com>
2018-10-25 14:55:00 +0800

    Initial commit

---
+++ runserver.py
@@ -0,0 +1,9 @@
+from kmu_econ import app
+
+# 使用路由 为URL 绑定视图函数
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
